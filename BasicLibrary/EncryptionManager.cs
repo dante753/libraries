065185fef91e8d9ba2fac4bf3ade6154091ac1ea
dante753 <francis.mendez@hotmail.com>
2020-11-22 20:17:12 -0600

    Repository with basic classes in c #

---
+++ BasicLibrary/EncryptionManager.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace BasicLibrary
+{
+    public class EncryptionManager
+    {
+        public class AesEncription
+        {
+            public string DecryptedResult;
+            public string EncryptedResult;
+
+            private byte[] IV = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
+            private int BlockSize = 128;
+
+            public bool Encrypt(string TextToEncrypt)
+            {
+                if (!string.IsNullOrEmpty(TextToEncrypt))
+                {
+                    try
+                    {
+                        byte[] bytes = Encoding.Unicode.GetBytes(TextToEncrypt);
+                        //Encrypt
+                        SymmetricAlgorithm crypt = Aes.Create();
+                        HashAlgorithm hash = MD5.Create();
+                        crypt.BlockSize = BlockSize;
+                        crypt.Key = hash.ComputeHash(Encoding.Unicode.GetBytes(TextToEncrypt));
+                        crypt.IV = IV;
+
+                        using (MemoryStream memoryStream = new MemoryStream())
+                        {
+                            using (CryptoStream cryptoStream =
+                               new CryptoStream(memoryStream, crypt.CreateEncryptor(), CryptoStreamMode.Write))
+                            {
+                                cryptoStream.Write(bytes, 0, bytes.Length);
+                            }
+
+                            EncryptedResult = Convert.ToBase64String(memoryStream.ToArray());
+                        }
+
+                        return true;
+                    }
+                    catch (Exception ex) { }
+                }
+
+                return false;
+            }
+            public bool Decrypt(string TextToDecrypt)
+            {
+                bool SecurityResult = false;
+                if (TextToDecrypt == "") return SecurityResult;
+
+                //Decrypt
+                byte[] bytes = Convert.FromBase64String(TextToDecrypt);
+                SymmetricAlgorithm crypt = Aes.Create();
+                HashAlgorithm hash = MD5.Create();
+                crypt.Key = hash.ComputeHash(Encoding.Unicode.GetBytes(TextToDecrypt));
+                crypt.IV = IV;
+
+                using (MemoryStream memoryStream = new MemoryStream(bytes))
+                {
+                    using (CryptoStream cryptoStream =
+                       new CryptoStream(memoryStream, crypt.CreateDecryptor(), CryptoStreamMode.Read))
+                    {
+                        byte[] decryptedBytes = new byte[bytes.Length];
+                        cryptoStream.Read(decryptedBytes, 0, decryptedBytes.Length);
+                        DecryptedResult = Encoding.Unicode.GetString(decryptedBytes);
+                        SecurityResult = true;
+                    }
+                }
+
+                return SecurityResult;
+            }
+
+
+        }
+
+        public class MD5Encription
+        {
+            public static string Get(string word)
+            {
+                MD5 md5 = MD5.Create();
+                ASCIIEncoding encoding = new ASCIIEncoding();
+                byte[] stream = null;
+                StringBuilder sb = new StringBuilder();
+                stream = md5.ComputeHash(encoding.GetBytes(word));
+                for (int i = 0; i < stream.Length; i++) sb.AppendFormat("{0:x2}", stream[i]);
+                return sb.ToString();
+            }
+        }
+
+        public class Base64Encription
+        {
+            public static string Encode(string word)
+            {
+                byte[] byt = Encoding.UTF8.GetBytes(word);
+                return Convert.ToBase64String(byt);
+            }
+
+            public static string Decode(string word)
+            {
+                byte[] b = Convert.FromBase64String(word);
+                return Encoding.UTF8.GetString(b);
+            }
+        }
+
+        public class SHA1Encription
+        {
+            public static string Get(string str)
+            {
+                SHA1 sha1 = SHA1.Create();
+                ASCIIEncoding encoding = new ASCIIEncoding();
+                byte[] stream = null;
+                StringBuilder sb = new StringBuilder();
+                stream = sha1.ComputeHash(encoding.GetBytes(str));
+                for (int i = 0; i < stream.Length; i++) sb.AppendFormat("{0:x2}", stream[i]);
+                return sb.ToString();
+            }
+        }
+
+        public class SHA256Encription
+        {
+            public static string Get(string str)
+            {
+                SHA256 sha256 = SHA256.Create();
+                ASCIIEncoding encoding = new ASCIIEncoding();
+                byte[] stream = null;
+                StringBuilder sb = new StringBuilder();
+                stream = sha256.ComputeHash(encoding.GetBytes(str));
+                for (int i = 0; i < stream.Length; i++) sb.AppendFormat("{0:x2}", stream[i]);
+                return sb.ToString();
+            }
+        }
+
+        public class SHA384Encription
+        {
+            public static string Get(string str)
+            {
+                SHA384 sha384 = SHA384.Create();
+                ASCIIEncoding encoding = new ASCIIEncoding();
+                byte[] stream = null;
+                StringBuilder sb = new StringBuilder();
+                stream = sha384.ComputeHash(encoding.GetBytes(str));
+                for (int i = 0; i < stream.Length; i++) sb.AppendFormat("{0:x2}", stream[i]);
+                return sb.ToString();
+            }
+        }
+
+        public class SAH256Encription
+        {
+            public static string Get(string str)
+            {
+                SHA512 sha512 = System.Security.Cryptography.SHA512.Create();
+                ASCIIEncoding encoding = new ASCIIEncoding();
+                byte[] stream = null;
+                StringBuilder sb = new StringBuilder();
+                stream = sha512.ComputeHash(encoding.GetBytes(str));
+                for (int i = 0; i < stream.Length; i++) sb.AppendFormat("{0:x2}", stream[i]);
+                return sb.ToString();
+            }
+        }
+
+    }
+}
---
+++ BasicLibrary/SqlConnDataBase.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace BasicLibrary
+{
+    public class SqlConnDataBase : IDisposable
+    {
+        /// <summary>
+        /// Store the Connection String
+        /// </summary>
+        private string strConn = null;
+        private SqlCommand Command = null;
+        private SqlConnection Connection = null;
+        private SqlDataAdapter DataAdapter = null;
+        private DataTable Table { get; set; }
+
+        /// <summary>
+        /// Return a Message as result of any action
+        /// </summary>
+        public string Message { get; private set; }
+        /// <summary>
+        /// Return the state of the connection
+        /// </summary>
+        public bool State { get; private set; }
+        // Track whether Dispose has been called.
+        private bool isDisposed = false;
+
+        //Constructor
+        /// <summary>
+        /// Implement the Connection to SQL by ADO
+        /// </summary>
+        /// <param name="_ConnectionString">Set the name of key on the Web Config or set the complete connection string</param>
+        /// <param name="FromWebConfig">If true, the connection string is gived from a WebConfig file</param>
+        public SqlConnDataBase(string _ConnectionString, bool FromWebConfig = false)
+        {
+            if (FromWebConfig)
+                strConn = ConfigurationManager.ConnectionStrings[_ConnectionString].ConnectionString;
+            else
+                strConn = _ConnectionString;
+
+            //init
+            try
+            {
+                Connection = new System.Data.SqlClient.SqlConnection(strConn);
+                Connection.Open();
+                State = true;
+            }
+            catch(Exception ex)
+            {
+                State = false;
+                //Message = "No se ha establecido la conexión con la base de datos.";
+                Message = "The connection to the database has not been established.";
+            }
+        }
+
+        /// <summary>
+        /// Return a table with the query result
+        /// </summary>
+        /// <returns></returns>
+        public DataTable GetTable()
+        {
+            return Table;
+        }
+
+        /// <summary>
+        /// Execute the SqlCommand expecting something back
+        /// </summary>
+        /// <returns></returns>
+        public bool ReadExecution()
+        {
+            if (State)
+            {
+                try
+                {
+                    DataSet _DataSet = new DataSet();
+                    DataAdapter = new SqlDataAdapter(Command);
+                    DataAdapter.Fill(_DataSet);
+
+                    //One table was returned and stored in Table variable
+                    Table = _DataSet.Tables[0];
+
+                    return true;
+                }
+                catch(Exception ex)
+                {
+                    //Message = "Error de ejecución";
+                    Message = "Execution error.";
+                    return false;
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Execute the SqlCommand with no read result
+        /// </summary>
+        /// <returns></returns>
+        public bool NoReadExecution()
+        {
+            if (State)
+            {
+                try
+                {
+                    Command.ExecuteNonQuery();
+
+                    return true;
+                }
+                catch(Exception ex)
+                {
+                    //Message = "Error de ejecución";
+                    Message = "Execution error.";
+                    return false;
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Set the name of an StoreProcedure to be executed
+        /// </summary>
+        /// <param name="myStoreProcedure">Name of the SP on SQL DataBase</param>
+        public void StoreProcedureName(string myStoreProcedure)
+        {
+            Command = new SqlCommand(myStoreProcedure, Connection);
+            Command.CommandType = CommandType.StoredProcedure;
+        }
+        /// <summary>
+        /// Set the query string to be executed
+        /// </summary>
+        /// <param name="myQueryString">Set the query sintax to execute on DB</param>
+        public void Query(string myQueryString)
+        {
+            Command = new SqlCommand(myQueryString, Connection);
+            Command.CommandType = CommandType.Text;
+        }
+
+        /// <summary>
+        /// Used with the StoreProcedureName to set the parameters of the StoreProcedure
+        /// </summary>
+        /// <param name="ParamName">Set the name of the Parameter like "@ParamName". This must to be equal in the SP on DataBase</param>
+        /// <param name="ParamVal">Set the value associated with the parameter</param>
+        public void InputParam(string ParamName, string ParamVal)
+        {
+            Command.Parameters.AddWithValue(ParamName, ParamVal).Direction = ParameterDirection.Input;
+        }
+        public void InputParam(string ParamName, int ParamVal)
+        {
+            Command.Parameters.AddWithValue(ParamName, ParamVal).Direction = ParameterDirection.Input;
+        }
+        public void InputParam(string ParamName, bool ParamVal)
+        {
+            Command.Parameters.AddWithValue(ParamName, ParamVal).Direction = ParameterDirection.Input;
+        }
+
+
+
+        public void Dispose()
+        {
+            if(Connection.State == ConnectionState.Open)
+            {
+                Connection.Close();
+            }
+
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~SqlConnDataBase()
+        {
+            // Finalizer calls Dispose(false)
+            Dispose(false);
+        }
+
+        // The bulk of the clean-up code is implemented in Dispose(bool)
+        protected virtual void Dispose(bool disposing)
+        {
+            if (isDisposed)
+                return;
+            if (disposing)
+            {
+                // if any managed resources to be free.  
+                if (Connection.State == ConnectionState.Open)
+                {
+                    Connection.Close();
+                }
+            }
+            isDisposed = true;
+        }
+    }
+}
